2ac987bf399195440994b4a5c9e4da34f1d1c5a4
Erick <eaocandos@gmail.com>
2020-12-21 11:50:07 -0500

    Se cambia proyecto a uno en blanco generado en la pagina de spring boot

--- src/main/java/co/com/pt/enums/CodeEnum.java
+++
@@ -1,5 +0,0 @@
-package co.com.pt.enums;
-
-public interface CodeEnum {
-    Integer getCode();
-}
--- src/main/java/co/com/pt/shared/Views.java
+++
@@ -1,8 +0,0 @@
-package co.com.pt.shared;
-
-public class Views {
-
-    public static class Messages { }
-    public static class NoCities { }
-
-}
